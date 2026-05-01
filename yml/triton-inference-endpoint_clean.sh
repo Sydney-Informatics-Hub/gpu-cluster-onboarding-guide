f27686526640b21bcbf9e1fd0f894e740c357119
Angus Fisk <angus_fisk@hotmail.com>
2026-05-01 21:14:11 +1000

    Openai compatible triton endpoint
    Creates a workspace with openai compatible endpoint, which is required
    for openwebui to work.
    
    Important notes
    - Min-replicas as 1 for now as running into bug when re-loading after
      scale up. The previous model weights are not purged from memory, so
      tries to reload model weights again and causes OOM error.
    - bash .. cd ... python main.py runs the main.py script in the
      openai-frontend dir of the triton server. converts the v2/generate and
      v2/repository/index into openai compatible frontend with v1/models and
      v1/generate
    - sets serving-port to be 9000, apparently important for the openai
      compatilbe ui

--- yml/triton-inference-endpoint_clean.sh
+++ yml/triton-inference-endpoint_clean.sh
@@ -11,9 +11,9 @@ runai inference submit triton-vllm1 \
   --existing-pvc claimname=pvc-rds-core-sih4hpc-rw,path=/scratch/pvc-rds-core-sih4hpc-rw \
   -e HF_HOME=/scratch/pvc-rds-core-sih4hpc-rw/huggingface \
   -e HUGGING_FACE_HUB_TOKEN=hf_xxxxxxx \
-  --serving-port container=8000,protocol=http \
+  --serving-port container=9000,protocol=http \
   --min-replicas 1 \
   --max-replicas 1 \
   --initialization-timeout-seconds 600 \
-  -- tritonserver --model-repository=/scratch/pvc-rds-core-sih4hpc-rw/model_repository
+  -- bash -c "cd /opt/tritonserver/python/openai && python3 openai_frontend/main.py --model-repository=/scratch/pvc-rds-core-sih4hpc-rw/model_repository --tokenizer Qwen/Qwen3-4B"
 
